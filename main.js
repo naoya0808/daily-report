b03e23e72a7c5156fb188c6486a84d1caa307350
naoya0808 <tokunao1124@icloud.com>
2024-06-26 22:33:52 +0900

    完成

---
+++ main.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import { fetchHistoryData } from "./my-modules/fetch-history-data";
+import { submitData } from "./my-modules/submit-data";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.APP_ID
+  };
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+
+if (document.getElementById("js-history")) {
+  fetchHistoryData(getDocs, collection, db);
+}
+
+if (document.getElementById("js-form")) {
+  document.getElementById("js-form").addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
+}
